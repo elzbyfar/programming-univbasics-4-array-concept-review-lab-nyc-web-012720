8ccf47391080f596fd61e68d48f8718dc22cbc53
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-09 19:25:10 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,22 +8,28 @@ def find_element_index(array, value_to_find)
 end
 
 
-def find_max_value(array)
+def find_max_value(array) 
+  max = array[0]
   array.length.times do |x|
-    if array[x] == array.max 
-      return array[x]
+    if array[x] > max 
+      max = array[x]
     end
   end
+    max
 end
-
 
 def find_min_value(array)
+  min = array[0]
   array.length.times do |x|
-    if array[x] == array.min 
-      return array[x]
+    if array[x] < min 
+      min = array[x]
     end
   end
+min
 end
+
+
+
 
 
 
@@ -64,20 +70,37 @@ end
   
 # # # *** SECOND ALTERNATIVE ***
 #   # array.index(value_to_find)
+
+
+# # # *** THIRD ALTERNATIVE ***
+#   # x = 0 
+#   # until x >= array.length do
+#   #   if array[x] == value_to_find
+#   #     return x 
+#   #   end 
+#   #   x+=1 
+#   # end 
+#   # nil 
+
 # end
 
 
 
 
+
+
 # def find_max_value(array)
-  
-#   # *** PREFERRED ***
+
+# # # *** PREFERRED ***
+#   max = array[0]
 #   array.length.times do |x|
-#     if array[x] == array.max 
-#       return array[x]
+#     if array[x] > max 
+#       max = array[x]
 #     end
 #   end
-  
+#     max
+
+
 # # # *** ALTERNATIVE ***
 #   # array.max
   
@@ -95,6 +118,38 @@ end
 #   #   end
 #   #   x +=1 
 #   # end
+
+
+# # # *** FOURTH ALTERNATIVE ***
+#   # array.length.times do |x|
+#   #   if array[x] == array.max 
+#   #     return array[x]
+#   #   end
+#   # end
+
+
+# # # *** FIFTH ALTERNATIVE ***
+#   # max = array[0]
+#   # x = 0 
+#   # while x < array.length do
+#   #   if array[x] > max 
+#   #     max = array[x]
+#   #   end
+#   #   x += 1 
+#   # end
+#   # max
+  
+  
+# # # *** SIXTH ALTERNATIVE ***
+#   # max = array[0]
+#   # x = 0 
+#   # until x >= array.length do 
+#   #   if array[x] > max 
+#   #     max = array[x]
+#   #   end 
+#   #   x += 1 
+#   # end
+#   # max
   
 # end
 
@@ -105,11 +160,13 @@ end
 
 # def find_min_value(array)
 # # # *** PREFERRED ***
+#   min = array[0]
 #   array.length.times do |x|
-#     if array[x] == array.min 
-#       return array[x]
+#     if array[x] < min 
+#       min = array[x]
 #     end
 #   end
+# min
   
   
 # # # *** ALTERNATIVE ***
@@ -129,4 +186,35 @@ end
 # # # *** THIRD ALTERNATIVE ***
 #   # sorted_arr = array.sort 
 #   # sorted_arr[0]
+
+
+# # # *** FOURTH ALTERNATIVE ***
+#   # min = array[0]
+#   # x = 0 
+#   # while x < array.length do 
+#   #   if array[x] < min
+#   #     min = array[x]
+#   #   end 
+#   #   x += 1 
+#   # end
+#   # min
+
+# # # *** FIFTH ALTERNATIVE ***
+#   # min = array[0]
+#   # x = 0 
+#   # until x >= array.length do 
+#   #   if array[x] < min 
+#   #     min = array[x]
+#   #   end 
+#   #   x += 1 
+#   # end
+#   #   min
+
+# # # *** SIXTH ALTERNATIVE ***
+#   # array.length.times do |x|
+#   #   if array[x] == array.min 
+#   #     return array[x]
+#   #   end
+#   # end
+
 # end
